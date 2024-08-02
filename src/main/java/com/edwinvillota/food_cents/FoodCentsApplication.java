bb535eb5e0c13a21ecc8992d39674630bc439287
edwinvillota <edwinvillota@hotmail.com>
2024-08-01 22:57:22 -0500

    FC-001: Initial commit

---
+++ src/main/java/com/edwinvillota/food_cents/FoodCentsApplication.java
@@ -0,0 +1,13 @@
+package com.edwinvillota.food_cents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edwinvillota/food_cents/FoodCentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwinvillota.food_cents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
