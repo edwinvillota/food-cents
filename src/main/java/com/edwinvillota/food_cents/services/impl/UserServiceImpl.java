18836575319c56cdbe5848db805deba7b4353ecf
edwinvillota <edwinvillota@hotmail.com>
2024-08-14 22:57:48 -0500

    FC-006: Change database and implemente category feature

--- src/main/java/com/edwinvillota/food_cents/services/impl/UserServiceImpl.java
+++ src/main/java/com/edwinvillota/food_cents/services/impl/UserServiceImpl.java
@@ -2,18 +2,20 @@ package com.edwinvillota.food_cents.services.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edwinvillota.food_cents.entities.User;
 import com.edwinvillota.food_cents.repositories.UserRepository;
-import com.edwinvillota.food_cents.services.UsersService;
+import com.edwinvillota.food_cents.services.UserService;
 
 @Service
-public class UsersServiceImpl implements UsersService {
+public class UserServiceImpl implements UserService {
 
-  @Autowired
-  private UserRepository userRepository;
+  private final UserRepository userRepository;
+
+  public UserServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
   @Override
   public List<User> findAll() {
